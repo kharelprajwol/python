f547b46205b86a7455fe736ae45dfe8964b2a3d2
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-09-25 22:34:50 +0545

    login users feature

--- 03-projects/1-django-authentication/AuthenticationProject/Core/urls.py
+++ 03-projects/1-django-authentication/AuthenticationProject/Core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Home, name="home"),
-    path("register", views.RegisterView, name="register"),
-    path("login", views.LoginView, name="login")
+    path("register/", views.RegisterView, name="register"),
+    path("login/", views.LoginView, name="login")
 ]
