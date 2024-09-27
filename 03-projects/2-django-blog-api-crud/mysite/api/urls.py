dea342fe83177f83760213652da019f5f888f9db
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-09-27 13:31:11 +0545

    completed using drf

--- 03-projects/2-django-blog-api-crud/mysite/api/urls.py
+++ 03-projects/2-django-blog-api-crud/mysite/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+    path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update")
 
 ]
--- 03-projects/2-django-blog-api-crud/mysite/api/views.py
+++ 03-projects/2-django-blog-api-crud/mysite/api/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+
 
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+
+    # CUSTOM
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_content)
+
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
+
+
+"""
+we can also create custom class by inheriting APIView from rest_framework
+"""
+
+
 
 
 
